dab163f632e86a202399c2efec7a8c2ba939e184
nastyabodnar <bodnar.post@gmail.com>
2020-08-28 02:13:20 +0600

    feat: add new console.log

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import _ from "lodash";
-_.intersection([2, 1], [2, 3]);
-_.capitalize('hello')';
+console.log(_.intersection([2, 1], [2, 3]));
+console.log(_.capitalize('hello'));
 
-console.log('hello Hexlet!');
 console.log(_.last(['one', 'two']));
 
